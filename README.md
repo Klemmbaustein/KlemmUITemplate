932902901099a8d2e45c588ac923037ecfe812bf
Klemmbaustein <jan.paschke@outlook.de>
2024-06-23 20:30:37 +0200

    added constants, changed default generator to visual studio

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ A project template for the [KlemmUI UI library](https://github.com/Klemmbaustein
 | ui/              | Contains .kui UI element definitions |
 | dependencies/    | Contains dependency submodules       |
 | app/             | Contains app files                   |
+
+
+On Windows, this template uses the `Visual Studio 17 2022` generator by default. This is because ninja doesn't check for files to build after running a command.
+So the program would need to be rebuilt twice for any changes to the markup to show up.
--- src/ui/MainElement.hpp
+++ src/ui/MainElement.hpp
@@ -11,20 +11,20 @@ public:
 	KlemmUI::UIText* unnamed_0;
 	MainElement() : UIBox(true)
 	{
-	this->SetPosition(Vector2f(-1,-1));
+	this->SetPosition(Vector2f(float(-1), float(-1)));
 	{
 	auto* e_1 = new KlemmUI::UIBackground(true, 0, 1);
-	e_1->SetColor(Vector3f(0.05,0.05,0.1));
-	e_1->SetMinSize(Vector2f(2,2));
-	e_1->SetMaxSize(Vector2f(2,2));
+	e_1->SetColor(Vector3f(float(0.05), float(0.05), float(0.15)));
+	e_1->SetMinSize(Vector2f(float(2), float(2)));
+	e_1->SetMaxSize(Vector2f(float(2), float(2)));
 	e_1->SetVerticalAlign(KlemmUI::UIBox::Align::Centered);
 	e_1->SetHorizontalAlign(KlemmUI::UIBox::Align::Centered);
 	this->AddChild(e_1);
 	{
 	auto* e_2 = new KlemmUI::UIText(1, 1, "", nullptr);
 	e_2->SetText(Message);
-	e_2->SetColor(Vector3f(1));
-	e_2->SetTextSize(40);
+	e_2->SetColor(Vector3f(float(1)));
+	e_2->SetTextSize(float(40));
 	e_2->SetTextSizeMode(KlemmUI::UIBox::SizeMode::PixelRelative);
 	e_2->SetFont(KlemmUI::MarkupLanguageManager::GetActive()->GetFont(""));
 	e_1->AddChild(e_2);
